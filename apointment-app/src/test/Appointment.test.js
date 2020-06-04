d29e15117182ede0629c188c1d3e5c9e3b8cf0fa
chakraborty <chakraborty@silexica.com>
2020-06-04 12:41:15 +0200

    Create appointments app with TDD

---
+++ apointment-app/src/test/Appointment.test.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment, AppointmentsDayView } from "../component/Appointment";
+describe('Appointment', () => {
+
+    let container;
+    let customer;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+
+    const render = component => ReactDOM.render(component, container);
+
+
+    it('renders the customer first name', () => {
+
+        customer = { firstName: 'Ashley' };
+
+        render(<Appointment customer={customer} />)
+        expect(container.textContent).toMatch('Ashley');
+    });
+
+    it('renders another customer first name', () => {
+        customer = { firstName: 'Jordan' };
+
+        render(<Appointment customer={customer} />)
+        expect(container.textContent).toMatch('Jordan');
+    });
+
+});
+
+
+describe('AppointmentsDayView', () => {
+    let container;
+    const today = new Date();
+    const appointments = [
+        { startsAt: today.setHours(12, 0) },
+        { startsAt: today.setHours(13, 0) }
+    ];
+    beforeEach(() => {
+        container = document.createElement('div');
+
+    });
+
+    const render = component =>
+        ReactDOM.render(component, container);
+
+    it('renders a div with the right id', () => {
+        render(<AppointmentsDayView appointments={[]} />);
+        expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+    });
+
+    it('renders multiple appointments in an ol element', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelector('ol')).not.toBeNull();
+        expect(
+            container.querySelector('ol').children
+        ).toHaveLength(2);
+    });
+
+    it('renders each appointment in an li', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelectorAll('li')).toHaveLength(2);
+        expect(
+            container.querySelectorAll('li')[0].textContent
+        ).toEqual('12:00');
+        expect(
+            container.querySelectorAll('li')[1].textContent
+        ).toEqual('13:00');
+    });
+});
